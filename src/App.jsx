72524ed73b2737dbe0b5434cf8d1d45f9a27e686
Ignacio Cuadra <ignacio.cuadra.z@gmail.com>
2022-10-02 14:47:15 -0300

    table

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import Card from './components/tfmc/Card'
+import Table from './components/tfmc/Table'
 
 function App() {
   return (
     <div className="h-full bg-slate-300 p-5">
       <div className='container mx-auto'>
-        <div className="w-[400px]">
+        <div className="my-5 w-[400px]">
           <Card />
+        </div>
+        <div className="my-5">
+          <Table />
         </div>
       </div>
     </div>
